055ebe900670119d2b8e93c428054c12b34ba62d
lilde90 <leepan1990@gmail.com>
2015-04-23 22:54:09 +0800

    modify two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -10,4 +10,30 @@
 //Input: numbers={2, 7, 11, 15}, target=9
 //Output: index1=1, index2=2
 //
+ass Solution {
+    public:
+        vector<int> twoSum(vector<int> &numbers, int target) {
+            vector<int> sumVec;
+            vector<int> indexVec;
+            vector<int> num = numbers;
+            if(numbers.size() < 2) return sumVec;
+            sort(numbers.begin(), numbers.end());
+            int i = 0;
+            int j = numbers.size() - 1;
+            while(i < j) {
+                if(numbers[i] + numbers[j] == target) {
+                    sumVec.push_back(numbers[i]);
+                    sumVec.push_back(numbers[j]);
+                    break;
+                }
+                else if(numbers[i] + numbers[j] < target) i++;
+                else j--;
+            }
+            if(sumVec.size() < 2) return indexVec;
+            for(int i = 0 ; i < num.size(); i++) {
+                if(num[i] == sumVec[0]  || num[i] == sumVec[1]) indexVec.push_back(i+1);
 
+            }
+            return indexVec;
+        }
+};
